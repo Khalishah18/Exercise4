b3329fc2ee357eb8f3bef126e626f6e7b99cb852
LISA <ASUS@LAPTOP-67G15PD7>
2022-12-05 15:50:28 +0700

    add stack menu

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -59,5 +59,14 @@ namespace Exercise4
                 }
             }
         }
+        static vooid Main(string[] args)
+        {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+            }
+        }
     }
 }
